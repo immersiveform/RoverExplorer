974d8a224fd4c5c4d1e4bf5fdbcec2d101c5e059
Lukasz D <dudziakl@users.noreply.github.com>
2020-09-27 12:18:09 +0200

    Initial commit

---
+++ Assets/MRTK.Tutorials.GettingStarted/Scripts/DirectionalIndicatorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace MRTK.Tutorials.GettingStarted
+{
+    public class DirectionalIndicatorController : MonoBehaviour
+    {
+        private void OnBecameInvisible()
+        {
+            // Triggered when 'DirectionalIndicator' component disables the Mesh Renderer
+            gameObject.SetActive(false);
+        }
+    }
+}
